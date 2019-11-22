f11cd7e9d728c19fbdc1000ecf4c956730ef3171
fengerleng <49905655+fengerleng@users.noreply.github.com>
2019-11-22 16:48:52 +0800

    Revert "我的云端同步康同学的云端"

--- GitTest/src/Test/TestGit.java
+++ GitTest/src/Test/TestGit.java
@@ -3,7 +3,7 @@ package Test;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("GitTest");
-		System.out.println("aaaa");
+		System.out.println("我添加了一行输出");
 		System.out.println();
 	}
 }
